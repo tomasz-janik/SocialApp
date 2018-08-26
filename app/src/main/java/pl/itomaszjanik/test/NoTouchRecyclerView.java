fffb1448d4f07684f75d0f0acaad0c6b57f6e206
tomasz-janik <tomasz.janik98@gmail.com>
2018-08-26 23:35:27 +0200

    Fixed scrolling inside NoteDetails in RecyclerView (disabled RecyclerView scroll)

---
+++ app/src/main/java/pl/itomaszjanik/test/NoTouchRecyclerView.java
@@ -0,0 +1,27 @@
+package pl.itomaszjanik.test;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NoTouchRecyclerView extends RecyclerView {
+
+    public NoTouchRecyclerView(Context context) {
+        super(context);
+    }
+
+    public NoTouchRecyclerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return false;
+    }
+}
