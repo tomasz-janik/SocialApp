810e2eeb19ede3c42d3b465df38678053ce9f009
tomasz-janik <tomasz.janik98@gmail.com>
2018-09-23 21:05:27 +0200

    Little bug fixes

---
+++ app/src/main/java/pl/itomaszjanik/test/Values.java
@@ -0,0 +1,31 @@
+package pl.itomaszjanik.test;
+
+public class Values {
+    static final int NO_OF_TABS = 5;
+    static final int INDEX_FEED = 0;
+    static final int INDEX_ADD = 1;
+    static final int INDEX_SEARCH = 2;
+    static final int INDEX_TOP = 3;
+    static final int INDEX_PROFILE = 4;
+
+    public static final int DAILY = 0;
+    public static final int WEEKLY = 1;
+    public static final int MONTHLY = 2;
+    public static final int ALLTIME = 3;
+    public static final int COMMENTED = 4;
+
+    public static final int MAX_TAGS_NUMBER = 5;
+
+    static final int COMMENT_LIMIT = 1000;
+    public static final int NOTE_VISIBLE_LIMIT = 300;
+
+    static final int COMMENT_EMPTY = -1;
+    static final int COMMENT_TOO_LONG = -2;
+
+    public static final int REQUEST_FULLSCREEN = 1;
+
+    public static final String SHARED_PREFERENCES = "preferences";
+
+    public static final String URL = "https://disapproved-visitor.000webhostapp.com/";
+
+}
